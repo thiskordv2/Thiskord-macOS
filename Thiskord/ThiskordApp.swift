ad80965be202c94e9db07c4c7d02b7d1f06ead76
FisaroJuju <68395401+FisaroJuju@users.noreply.github.com>
2026-05-12 10:41:27 +0200

    Initial Commit

---
+++ Thiskord/ThiskordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThiskordApp.swift
+//  Thiskord
+//
+//  Created by Julien Ejupi on 12/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ThiskordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
